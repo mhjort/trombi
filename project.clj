b854b9dce0a85ea0b96164a9bef84afaf3d88996
Markus Hjort <markus.hjort@reaktor.fi>
2014-01-16 07:03:25 +0200

    Add unique process id

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]]
-  :main clj-gatling.core/main)
+  :main clj-gatling.core)
--- src/clj_gatling/core.clj
+++ src/clj_gatling/core.clj
@@ -1,14 +1,16 @@
 (ns clj-gatling.core
-  (:require [clojure.core.async :as async :refer [go <! >!]]))
+  (:require [clojure.core.async :as async :refer [go <! >!]])
+  (:gen-class))
 
 (defn process [id]
-  (println "executing")
+  (println (str "executing" id))
   (str "processed " id))
 
-(defn main [threads]
+(defn -main [threads]
   (println (str "Run simulation with " threads " threads"))
-  (let [cs (repeatedly (read-string threads) async/chan)]
-    (doseq [c cs] (go (>! c (process 1))))
+  (let [cs (repeatedly (read-string threads) async/chan)
+        ps (map vector (iterate inc 1) cs)]
+    (doseq [[i c] ps] (go (>! c (process i))))
     (dotimes [i (read-string threads)]
       (let [[v c] (async/alts!! cs)] 
         (println v)))
