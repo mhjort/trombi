f17fa7626de2649cc8bb0ae7e73d6108f654944a
Markus Hjort <markus.hjort@reaktor.fi>
2014-01-24 20:23:33 +0200

    Add gatling as dependency

--- project.clj
+++ project.clj
@@ -4,5 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+                 [io.gatling/gatling-charts "2.0.0-M3a"]
+                 [io.gatling.highcharts/gatling-charts-highcharts "2.0.0-M3a"]]
+  :repositories { "excilys" "http://repository.excilys.com/content/groups/public" } 
   :main clj-gatling.core)
--- src/clj_gatling/core.clj
+++ src/clj_gatling/core.clj
@@ -1,4 +1,5 @@
 (ns clj-gatling.core
+  (:import (io.gatling.charts.report ReportsGenerator))
   (:require [clojure.core.async :as async :refer [go <! >!]])
   (:gen-class))
 
@@ -6,12 +7,16 @@
   (println (str "executing" id))
   (str "processed " id))
 
-(defn -main [threads]
+(defn run-simulation [threads]
   (println (str "Run simulation with " threads " threads"))
-  (let [cs (repeatedly (read-string threads) async/chan)
+  (let [cs (repeatedly threads async/chan)
         ps (map vector (iterate inc 1) cs)]
     (doseq [[i c] ps] (go (>! c (process i))))
-    (dotimes [i (read-string threads)]
+    (dotimes [i threads]
       (let [[v c] (async/alts!! cs)] 
         (println v)))
     (doseq [c cs] (async/close! c))))
+
+(defn -main [threads]
+  (run-simulation (read-string threads)))
+;  (ReportsGenerator/generateFor "out" nil))
