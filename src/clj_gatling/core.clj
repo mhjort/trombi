db3ebc1d59fba998142854c19df81a11c62bb898
Markus Hjort <markus.hjort@reaktor.fi>
2014-02-02 22:48:34 +0200

    Add possibility to run multiple requests in one scenario

--- src/clj_gatling/core.clj
+++ src/clj_gatling/core.clj
@@ -1,4 +1,5 @@
 (ns clj-gatling.core
+  (:use [clojure.set :only [rename-keys]])
   (:import (io.gatling.charts.report ReportsGenerator)
            (io.gatling.charts.result.reader FileDataReader)
            (io.gatling.core.config GatlingConfiguration))
@@ -6,24 +7,31 @@
             [clojure-csv.core :as csv])
   (:gen-class))
 
+(def test-scenario {:requests ["Request1" "Request2"]})
+
 (defmacro bench [expr]
   `(let [start# (System/currentTimeMillis)
          result# ~expr]
       {:result result# :start start# :end (System/currentTimeMillis) }))
 
-(defn request [id]
-  (println (str "Simulating request for " id))
-  (Thread/sleep (rand 1000)))
+(defn run-request [name id]
+  ;(println (str "Simulating request for " id))
+  (Thread/sleep (rand 1000))
+  "OK")
 
-(defn process [id]
-  (let [bench-result (bench (request id))]
-    {:id id :start (:start bench-result) :end (:end bench-result)}))
+(defn run-scenario [scenario id]
+  (assoc
+    (rename-keys 
+      (bench
+        (map #(assoc (bench (run-request % id)) :name % :id id) (:requests scenario)))
+      {:result :requests})
+    :id id))
 
 (defn run-simulation [threads]
   (println (str "Run simulation with " threads " threads"))
   (let [cs (repeatedly threads async/chan)
         ps (map vector (iterate inc 0) cs)]
-    (doseq [[i c] ps] (go (>! c (process i))))
+    (doseq [[i c] ps] (go (>! c (run-scenario test-scenario i))))
     (let [result (for [i (range threads)]
       (let [[v c] (async/alts!! cs)]
         v))]
@@ -37,20 +45,26 @@
     (GatlingConfiguration/setUp conf)
     (ReportsGenerator/generateFor "out" (FileDataReader. "23"))))
 
-(defn map-request [line]
-  (let [start (.toString (:start line))
-        end (.toString (:end line))]
-    ["REQUEST" "Scenario name" (.toString (:id line)) "" "request_1" start start "1390591841245" end "OK" "\u0020"]))
+(defn map-request [request]
+  (let [start (.toString (:start request))
+        end (.toString (:end request))]
+    ["REQUEST" "Scenario name" (.toString (:id request)) "" (:name request) start start "1390591841245" end "OK" "\u0020"]))
 
-(defn map-scenario [line]
-  (let [start (.toString (:start line))
-        end (.toString (:end line))]
-    ["SCENARIO"	"Scenario name"	(.toString (:id line)) start end]))
+(defn flatten-one-level [coll]  
+  (mapcat #(if (sequential? %) % [%]) coll))
+
+(defn map-scenario [scenario]
+  (let [start (.toString (:start scenario))
+        end (.toString (:end scenario))
+        requests (apply concat (map #(vector (map-request %)) (:requests scenario)))]
+    (conj requests ["SCENARIO" "Scenario name" (.toString (:id scenario)) start end])))
 
 (defn create-result-lines [result]
-  (let [header ["RUN" "20140124213040" "basicexamplesimulation" "\u0020"] 
-        reqs-and-scenarios (apply concat (map #(vector (map-request %) (map-scenario %)) result))]
-    (conj reqs-and-scenarios header)))
+  (let [header ["RUN" "20140124213040" "basicexamplesimulation" "\u0020"]
+        scenarios (apply concat (map #(vector (map-scenario %)) result))
+       result-lines (conj (flatten-one-level scenarios) header)]
+    (println result-lines)
+    result-lines))
 
 (defn -main [threads]
   (let [result (run-simulation (read-string threads))
