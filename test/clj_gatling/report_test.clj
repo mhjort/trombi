a7e81c93eb2ed075d4eb29cbc58cb141f79c167c
Markus Hjort <markus.hjort@reaktor.fi>
2014-03-11 19:39:17 +0200

    Add multiple scenarios to report test

--- test/clj_gatling/report_test.clj
+++ test/clj_gatling/report_test.clj
@@ -9,7 +9,9 @@
                {:id 1 :name "Request2" :start 1391936497299 :end 1391936497996 :result true}]}
    {:name "Test scenario" :id 0 :start 1391936496808 :end 1391936496808
     :requests [{:id 0 :name "Request1" :start 1391936497998 :end 1391936498426 :result true} 
-               {:id 0 :name "Request2" :start 1391936498430 :end 1391936498450 :result false}]}])
+               {:id 0 :name "Request2" :start 1391936498430 :end 1391936498450 :result false}]}
+   {:name "Test scenario2" :id 0 :start 1391936496808 :end 1391936496808
+    :requests [{:id 0 :name "Request1" :start 1391936497998 :end 1391936498426 :result true}]}])
 
 (def expected-lines
   [["RUN" "20140209110136" "simulation" "\u0020"] 
@@ -18,7 +20,9 @@
    ["REQUEST" "Test scenario" "1" "" "Request2" "1391936497299" "1391936497299" "1391936497996" "1391936497996" "OK" "\u0020"] 
    ["SCENARIO" "Test scenario" "0" "1391936496808" "1391936496808"] 
    ["REQUEST" "Test scenario" "0" "" "Request1" "1391936497998" "1391936497998" "1391936498426" "1391936498426" "OK" "\u0020"]
-   ["REQUEST" "Test scenario" "0" "" "Request2" "1391936498430" "1391936498430" "1391936498450" "1391936498450" "KO" "\u0020"]])
+   ["REQUEST" "Test scenario" "0" "" "Request2" "1391936498430" "1391936498430" "1391936498450" "1391936498450" "KO" "\u0020"]
+   ["SCENARIO" "Test scenario2" "0" "1391936496808" "1391936496808"] 
+   ["REQUEST" "Test scenario2" "0" "" "Request1" "1391936497998" "1391936497998" "1391936498426" "1391936498426" "OK" "\u0020"]])
 
 (defn run-line-match [expected actual]
   (is (= (first expected) (first actual))))
