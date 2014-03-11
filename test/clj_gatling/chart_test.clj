e68f177432ccc477afc4f0a814a2a623a7894f22
Markus Hjort <markus.hjort@reaktor.fi>
2014-03-11 18:26:47 +0200

    Delete generated chart file only if it exists already

--- test/clj_gatling/chart_test.clj
+++ test/clj_gatling/chart_test.clj
@@ -9,10 +9,14 @@
 (defn copy-file [source-path dest-path]
   (io/copy (io/file source-path) (io/file dest-path)))
 
+(defn delete-file-if-exists [path]
+  (when (.exists (io/as-file path)) 
+    (io/delete-file path)))
+
 (deftest creates-chart-from-simulation-file
   (create-dir "target/test-results/input")
   (copy-file "test/simulation.log" "target/test-results/input/simulation.log")
-  (io/delete-file "target/test-results/output/index.html")
+  (delete-file-if-exists "target/test-results/output/index.html")
   (chart/create-chart "target/test-results")
   (is (.exists (io/as-file "target/test-results/output/index.html"))))
 
