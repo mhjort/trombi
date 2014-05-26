d523e22a71dc6d78142058034a590daf9d0463e1
Markus Hjort <markus.hjort@reaktor.fi>
2014-05-26 22:09:30 +0300

    Add http-kit as dependency

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [clojure-csv/clojure-csv "2.0.1"]
+                 [http-kit "2.1.16"]
                  [clj-time "0.6.0"]
                  [io.gatling/gatling-charts "2.0.0-M3a"]
                  [io.gatling.highcharts/gatling-charts-highcharts "2.0.0-M3a"]]
