03569337389a04a789b936bfc43902c9d7034ec6
Markus Hjort <markus.hjort@reaktor.fi>
2014-10-20 22:03:59 +0300

    Upgrade Clojure and core.async

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://github.com/mhjort/clj-gatling"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-containment-matchers "0.9.1"]
-                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [clojure-csv/clojure-csv "2.0.1"]
                  [cheshire "5.3.1"]
                  [compojure "1.1.5"]
