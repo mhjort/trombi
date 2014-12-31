f09b2ed2c0256ba32049a5506fe24e4beaaf93a8
Markus Hjort <markus.hjort@reaktor.fi>
2014-12-31 09:37:42 +0200

    Move clj-containment-matchers to dev dependency and take latest version of it into use

--- project.clj
+++ project.clj
@@ -4,11 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-containment-matchers "0.9.1"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [clojure-csv/clojure-csv "2.0.1"]
                  [http-kit "2.1.16"]
                  [clj-time "0.8.0"]
                  [io.gatling/gatling-charts "2.0.3"]
                  [io.gatling.highcharts/gatling-charts-highcharts "2.0.3"]]
-  :repositories { "excilys" "http://repository.excilys.com/content/groups/public" })
+  :repositories { "excilys" "http://repository.excilys.com/content/groups/public" }
+  :profiles {:dev {:dependencies [[clj-containment-matchers "0.9.3"]] }})
