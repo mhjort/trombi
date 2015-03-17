f8579267222ab6e95efc7f7b7ceee89c70e927c0
Markus Hjort <markus.hjort@reaktor.fi>
2015-03-17 12:56:48 +0200

    Bump http-kit

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [clojure-csv/clojure-csv "2.0.1"]
-                 [http-kit "2.1.16"]
+                 [http-kit "2.1.18"]
                  [clj-time "0.8.0"]
                  [io.gatling/gatling-charts "2.0.3"]
                  [io.gatling.highcharts/gatling-charts-highcharts "2.0.3"]]
