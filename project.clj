cd889d2735cb0f090a916c4ea6f541bbb962d679
Markus Hjort <markus.hjort@reaktor.fi>
2015-04-15 20:29:04 +0300

    Exclude some transitive dependencies from Gatling. Those were not needed for generating charts.

--- project.clj
+++ project.clj
@@ -8,7 +8,12 @@
                  [clojure-csv/clojure-csv "2.0.1"]
                  [http-kit "2.1.18"]
                  [clj-time "0.8.0"]
-                 [io.gatling/gatling-charts "2.0.3"]
-                 [io.gatling.highcharts/gatling-charts-highcharts "2.0.3"]]
+                 [io.gatling/gatling-charts "2.0.3"
+                   :exclusions [com.typesafe.akka/akka-actor_2.10
+                                org.jodd/jodd-lagarto
+                                com.fasterxml.jackson.core/jackson-databind
+                                net.sf.saxon/Saxon-HE]]
+                 [io.gatling.highcharts/gatling-charts-highcharts "2.0.3"
+                   :exclusions [io.gatling/gatling-app io.gatling/gatling-recorder]]]
   :repositories { "excilys" "http://repository.excilys.com/content/groups/public" }
   :profiles {:dev {:dependencies [[clj-containment-matchers "0.9.3"]] }})
