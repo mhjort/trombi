4e853342a6b9850d629ae3f671d48fc18e9ea20d
Markus Hjort <markus.hjort@reaktor.fi>
2015-06-24 20:36:58 +0300

    Add minimal passing core.typed annotations

--- src/clj_gatling/core.clj
+++ src/clj_gatling/core.clj
@@ -1,13 +1,22 @@
 (ns clj-gatling.core
   (:import (org.joda.time LocalDateTime))
   (:require [clojure-csv.core :as csv]
+            [clojure.core.typed :as t]
             [clj-gatling.chart :as chart]
             [clj-gatling.report :as report]
             [clj-gatling.simulation :as simulation]))
 
-(defn create-dir [dir]
+(t/ann create-dir (t/IFn [String -> t/Any]))
+(defn- create-dir [^String dir]
   (.mkdirs (java.io.File. dir)))
 
+(t/ann ^:no-check clj-gatling.simulation/run-simulation (t/IFn [t/Any Long t/Any * -> t/Any]))
+(t/ann ^:no-check clj-gatling.report/create-result-lines (t/IFn [LocalDateTime t/Any -> t/Any]))
+(t/ann ^:no-check clj-gatling.chart/create-chart (t/IFn [t/Any -> t/Any]))
+(t/ann ^:no-check clojure.core/spit (t/IFn [t/Any * -> t/Any]))
+(t/ann ^:no-check clojure-csv.core/write-csv (t/IFn [t/Any * -> String]))
+
+(t/ann run-simulation (t/IFn [t/Any Long t/Any * -> t/Any]))
 (defn run-simulation [scenario users & [options]]
  (let [start-time (LocalDateTime.)
        results-dir (if (nil? (:root options))
