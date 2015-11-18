e359d1ee140f370a873d7c8bf307df234b35b9d6
Markus Hjort <markus.hjort@gmail.com>
2015-11-18 19:37:21 +0200

    Bump core.async

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [org.clojure/core.async "0.2.374"]
                  [clojure-csv/clojure-csv "2.0.1"]
                  [http-kit "2.1.19"]
                  [clj-time "0.11.0"]
