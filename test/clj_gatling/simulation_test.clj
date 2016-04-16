d29f746507e40b545b825afee4ed6ef5426c1806
Markus Hjort <markus.hjort@gmail.com>
2016-04-16 22:04:21 +0300

    Test basic simulation case using both old and new simulation schema

--- test/clj_gatling/simulation_test.clj
+++ test/clj_gatling/simulation_test.clj
@@ -15,13 +15,19 @@
       (recur (conj results result))
       results)))
 
-(defn- run-simulation [scenarios concurrency & [options]]
+(defn- run-legacy-simulation [scenarios concurrency & [options]]
   (let [step-timeout (or (:timeout-in-ms options) 5000)]
     (-> (simulation/run-scenarios {:runner (choose-runner scenarios concurrency options)
                                    :timeout step-timeout
                                    :context (:context options)}
                                   (weighted-scenarios (range concurrency) scenarios))
         to-vector)))
+
+(defn- run-single-scenario [scenario concurrency]
+  (to-vector (simulation/run {:name "Simulation"
+                              :scenarios [scenario]}
+                             {:concurrency concurrency
+                              :timeout 5000})))
 
 (def request-count (atom 0))
 
@@ -86,8 +92,8 @@
 (defn get-result [requests request-name]
   (:result (first (filter #(= request-name (:name %)) requests))))
 
-(deftest simulation-returns-result-when-run-with-one-user
-  (let [result (run-simulation [scenario] 1)]
+(deftest simulation-returns-result-when-run-with-one-user-with-legacy-format
+  (let [result (run-legacy-simulation [scenario] 1)]
     (is (equal? result [{:name "Test scenario"
                          :id 0
                          :start number?
@@ -103,10 +109,32 @@
                                      :end number?
                                      :result false}]}]))))
 
+(deftest simulation-returns-result-when-run-with-one-user
+  (let [result (run-single-scenario {:name "Test scenario"
+                                     :steps [{:name "Step1"
+                                              :action (fn [ctx] [true ctx])}
+                                             {:name "Step2"
+                                              :action (fn [ctx] [false ctx])}]}
+                                    1)]
+    (is (equal? result [{:name "Test scenario"
+                         :id 0
+                         :start number?
+                         :end number?
+                         :requests [{:name "Step1"
+                                     :id 0
+                                     :start number?
+                                     :end number?
+                                     :result true}
+                                    {:name "Step2"
+                                     :id 0
+                                     :start number?
+                                     :end number?
+                                     :result false}]}]))))
+
 (deftest when-function-returns-exception-it-is-handled-as-ko
   (let [s {:name "Exception scenario"
            :requests [{:name "Throwing" :fn error-throwing-request}]}
-        result (run-simulation [s] 1)]
+        result (run-legacy-simulation [s] 1)]
     (is (equal? result [{:name "Exception scenario"
                          :id 0
                          :start number?
@@ -118,7 +146,7 @@
                                      :result false}]}]))))
 
 (deftest simulation-passes-context-through-requests-in-scenario
-  (let [result (first (run-simulation [context-testing-scenario] 1))]
+  (let [result (first (run-legacy-simulation [context-testing-scenario] 1))]
     (is (= true (get-result (:requests result) "Request2")))))
 
 (deftest simulation-passes-original-context-to-first-request
@@ -126,13 +154,13 @@
                   :requests [{:name "Request1"
                               :fn (fn [cb {:keys [test-val]}]
                                     (cb (= 5 test-val)))}]}
-        result (first (run-simulation [scenario]
+        result (first (run-legacy-simulation [scenario]
                                       1
                                       {:context {:test-val 5}}))]
     (is (= true (get-result (:requests result) "Request1")))))
 
 (deftest simulation-skips-second-request-if-first-fails
-  (let [result (run-simulation [first-fails-scenario] 1)]
+  (let [result (run-legacy-simulation [first-fails-scenario] 1)]
     ;Note scenario is ran twice in this case to match number of handled requests which should be 2
     (is (equal? result
                 (repeat 2 {:name "Scenario"
@@ -146,7 +174,7 @@
                                        :result false}]})))))
 
 (deftest second-request-is-not-skipped-in-failure-if-skip-next-after-failure-is-unset
-  (let [result (run-simulation [(assoc first-fails-scenario :skip-next-after-failure? false)] 1)]
+  (let [result (run-legacy-simulation [(assoc first-fails-scenario :skip-next-after-failure? false)] 1)]
     (is (equal? result
                 [{:name "Scenario"
                            :id 0
@@ -165,13 +193,13 @@
 
 (deftest simulation-returns-result-when-run-with-http-requests
   (with-redefs [httpkit/async-http-request fake-async-http]
-    (let [result (first (run-simulation [http-scenario] 1))]
+    (let [result (first (run-legacy-simulation [http-scenario] 1))]
       (is (= "Test http scenario" (:name result)))
       (is (= true (get-result (:requests result) "Request1")))
       (is (= false (get-result (:requests result) "Request2"))))))
 
 (deftest simulation-returns-result-when-run-with-multiple-scenarios-with-one-user
-  (let [result (run-simulation [scenario scenario2] 2)]
+  (let [result (run-legacy-simulation [scenario scenario2] 2)]
     ;Stop condition is not synced between parallel scenarios
     ;so once in a while there might be one extra scenario
     ;This is ok tolerance for max requests
@@ -187,10 +215,10 @@
                                                 :requests anything}]))))
 
 (deftest throws-exception-when-concurrency-is-smaller-than-number-of-parallel-scenarios
-  (is (thrown? AssertionError (run-simulation [scenario scenario2] 1))))
+  (is (thrown? AssertionError (run-legacy-simulation [scenario scenario2] 1))))
 
 (deftest with-given-number-of-requests
-  (let [result (run-simulation [scenario] 1 {:requests 4})]
+  (let [result (run-legacy-simulation [scenario] 1 {:requests 4})]
     (is (= "Test scenario" (-> result first :name)))
     (is (= 2 (-> result first :requests count)))
     (is (= "Test scenario" (-> result second :name)))
@@ -198,17 +226,17 @@
 
 (deftest with-multiple-number-of-requests
   (reset! request-count 0)
-  (let [result (run-simulation [counting-scenario] 100 {:requests 2000})
+  (let [result (run-legacy-simulation [counting-scenario] 100 {:requests 2000})
         handled-requests (->> result (map :requests) count)]
     (is (approximately== handled-requests 2000))
     (is (= handled-requests @request-count))))
 
 (deftest duration-given
-  (let [result (run-simulation [scenario] 1 {:duration (time/millis 50)})]
+  (let [result (run-legacy-simulation [scenario] 1 {:duration (time/millis 50)})]
     (is (not (empty? result)))))
 
 (deftest fails-requests-when-they-take-longer-than-timeout
-  (let [result (first (run-simulation [timeout-scenario] 1 {:timeout-in-ms 100}))]
+  (let [result (first (run-legacy-simulation [timeout-scenario] 1 {:timeout-in-ms 100}))]
     (is (= false (get-result (:requests result) "Request1")))))
 
 (deftest scenario-weight
@@ -216,7 +244,7 @@
         request2 {:name "Request2" :fn successful-request}
         main-scenario {:name "Main" :weight 2 :requests [request1 request2]}
         second-scenario {:name "Second" :weight 1 :requests [request1]}
-        result (group-by :name (run-simulation
+        result (group-by :name (run-legacy-simulation
                                  [main-scenario second-scenario]
                                  10
                                  {:requests 100}))
