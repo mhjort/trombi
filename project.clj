438e1a554de4e86547e43fc8fab7cd3e08eae9e0
Markus Hjort <markus.hjort@gmail.com>
2016-04-23 18:26:32 +0300

    Bump Clojure version 1.7 -> 1.8

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/mhjort/clj-gatling"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.2.374"]
                  [clojure-csv/clojure-csv "2.0.1"]
                  [http-kit "2.1.19"]
