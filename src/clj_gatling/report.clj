62353302f9ca3f3032816114b9aaf16cc8d3327a
Markus Hjort <markus.hjort@gmail.com>
2018-02-15 15:04:26 +0200

    Simplify code

--- src/clj_gatling/report.clj
+++ src/clj_gatling/report.clj
@@ -1,11 +1,14 @@
 (ns clj-gatling.report
   (:require [schema.core :refer [validate]]
+            [clojure.set :refer [rename-keys]]
             [clj-gatling.schema :as schema]
-            [clojure.core.async :as a :refer [thread <!!]]))
+            [clojure.core.async :as a :refer [to-chan thread <!!]]))
 
 (defn create-result-lines [simulation buffer-size results-channel output-writer]
   (validate schema/Simulation simulation)
-  (let [summary (fn [result] (frequencies (mapcat #(map :result (:requests %)) result)))
+  (let [summary (fn [result]
+                  (rename-keys (frequencies (mapcat #(map :result (:requests %)) result))
+                               {true :ok false :ko}))
         ;Note! core.async/partition is deprecated function.
         ;This should be changed to use transducers instead
         results (a/partition buffer-size results-channel)
@@ -17,9 +20,8 @@
                                     (summary result))]
                             (recur (inc idx) (conj threads t)))
                           threads))]
-    (reduce (fn [m [k v]]
-              (if k
-                (update m :ok + v)
-                (update m :ko + v)))
-            {:ok 0 :ko 0}
+    (reduce (fn [m curr]
+              (println m curr)
+              (merge-with + m (apply hash-map curr)))
+            {}
             (mapcat #(<!! %) write-results))))
