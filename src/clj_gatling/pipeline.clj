d051ad2d3c32d27c2312b67b8ab4fb38d284f598
Markus Hjort <markus.hjort@gmail.com>
2018-02-19 11:01:17 +0200

    Run executor nodes in their own threads

--- src/clj_gatling/pipeline.clj
+++ src/clj_gatling/pipeline.clj
@@ -5,7 +5,7 @@
             [clj-gatling.simulation :as simu]
             [clj-gatling.simulation-util :refer [split-equally]]
             [clojure.set :refer [rename-keys]]
-            [clojure.core.async :refer [onto-chan to-chan chan go go-loop <!!]]))
+            [clojure.core.async :refer [thread <!!]]))
 
 (defn local-executor [node-id simulation-fn simulation options]
   (println "Starting local executor with id:" node-id)
@@ -16,14 +16,22 @@
         raw-summary (parse-in-batches simulation batch-size results reporters)]
     raw-summary))
 
+(defn prun [f users-by-node]
+  (let [results (loop [users-by-node users-by-node
+                       threads []]
+                  (if-let [users (first users-by-node)]
+                    (let [t (thread (f (count threads) users))]
+                      (recur (rest users-by-node) (conj threads t)))
+                    threads))]
+    (map #(<!! %) results)))
+
 (defn run [simulation
            {:keys [nodes executor concurrency reporters] :as options}]
   (let [users-by-node (split-equally nodes (range concurrency))
-        results-by-node (pmap #(executor %2
-                                         simulation-runner
-                                         simulation
-                                         (assoc options :users %1))
-                              users-by-node
-                              (range nodes))]
+        results-by-node (prun (fn [node-id users]
+                                (executor node-id
+                                          simulation-runner
+                                          simulation (assoc options :users users)))
+                              users-by-node)]
     (reduce (partial combine-with-reporters reporters)
             results-by-node)))
