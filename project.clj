3d90c17e083a0cc642c975d72c5c777b786403c0
Markus Hjort <markus.hjort@gmail.com>
2018-04-07 20:04:34 +0300

    Release 0.12.0-beta3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-gatling "0.12.0-beta2"
+(defproject clj-gatling "0.12.0-beta3"
   :description "Clojure library for load testing"
   :url "http://github.com/mhjort/clj-gatling"
   :license {:name "Eclipse Public License"
