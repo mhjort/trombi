ecd8e277e0388a54b2eb0f6b59f6d3646db0d3a1
Markus Hjort <markus.hjort@gmail.com>
2020-01-05 15:52:05 +0200

    Bump dependencies

--- project.clj
+++ project.clj
@@ -3,15 +3,15 @@
   :url "http://github.com/mhjort/clj-gatling"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.async "0.4.474"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/core.async "0.6.532"]
                  [http-kit "2.3.0"]
-                 [clj-time "0.14.4"]
-                 [prismatic/schema "1.1.9"]
+                 [clj-time "0.15.2"]
+                 [prismatic/schema "1.1.12"]
                  [clojider-gatling-highcharts-reporter "0.2.2"]]
   :profiles {:dev {:global-vars {*warn-on-reflection* false}
                    :source-paths ["examples"]
                    :dependencies [[clj-async-test "0.0.5"]
-                                  [org.clojure/test.check "0.9.0"]
+                                  [org.clojure/test.check "0.10.0"]
                                   [clj-containment-matchers "1.0.1"]] }}
   :aot [clj-time.core clj-gatling.simulation-runners])
