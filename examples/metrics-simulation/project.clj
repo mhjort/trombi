1e3de8f7b39cd411f0cdec9769f517b18fb92484
Markus Hjort <markus.hjort@gmail.com>
2021-07-21 15:42:33 +0300

    Add duration option to example

--- examples/metrics-simulation/project.clj
+++ examples/metrics-simulation/project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [clj-gatling "0.15.1"]]
+                 [clj-gatling "0.16.0"]]
   :main metrics-simulation.core)
--- examples/metrics-simulation/src/metrics_simulation/core.clj
+++ examples/metrics-simulation/src/metrics_simulation/core.clj
@@ -1,6 +1,7 @@
 (ns metrics-simulation.core
   (:require [clj-gatling.core :as gatling]
             [metrics-simulation.simulations :refer [simulations]])
+  (:import (java.time Duration))
   (:gen-class))
 
 (defn- ramp-up-distribution [percentage-at _]
@@ -9,23 +10,27 @@
     (< percentage-at 0.2) 0.2
     :else 1.0))
 
-(defn -main [simulation users requests & [option]]
+(defn -main [simulation users requests-or-duration & [option]]
   (let [simulation (or ((keyword simulation) simulations)
                        (throw (Exception. (str "No such simulation " simulation))))]
     (condp = option
+      "--with-duration" (gatling/run simulation
+                                 {:concurrency (read-string users)
+                                  :concurrency-distribution ramp-up-distribution
+                                  :duration (Duration/ofSeconds (read-string requests-or-duration))})
       "--no-report" (gatling/run simulation
                                  {:concurrency (read-string users)
                                   :concurrency-distribution ramp-up-distribution
                                   :reporter {:writer (fn [_ _ _])
                                              :generator (fn [simulation]
                                                           (println "Ran" simulation "without report"))}
-                                  :requests (read-string requests)})
+                                  :requests (read-string requests-or-duration)})
       "--ramp-up" (gatling/run simulation
                                {:concurrency (read-string users)
                                 :concurrency-distribution ramp-up-distribution
                                 :root "tmp"
-                                :requests (read-string requests)})
+                                :requests (read-string requests-or-duration)})
       (gatling/run simulation
                    {:concurrency (read-string users)
                     :root "tmp"
-                    :requests (read-string requests)}))))
+                    :requests (read-string requests-or-duration)}))))
