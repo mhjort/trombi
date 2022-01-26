4c9d4e1ec66f1a5936bc5bc78d2340387532d6f2
Dan Meyers <d.meyers@infinitefunspace.co.uk>
2022-01-26 14:56:39 +0000

    Catch `AssertionError` thrown by step-fn
    Fixes https://github.com/mhjort/clj-gatling/issues/81
    
    `(assert ...)` is a useful form for checking the status of a step, but
    `AssertionError` inherits from `Throwable`, not `Exception`. We don't
    want to catch _all_ throwables, as that includes things like
    `VirtualMachineError`, which indicate a fundamental issue where the only
    thing to do is exit.
    
    As such, we special-case `AssertionError` as a type to catch alongside
    `Exception`.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-gatling "0.17.1"
+(defproject clj-gatling "0.17.2"
   :description "Clojure library for load testing"
   :url "http://github.com/mhjort/clj-gatling"
   :license {:name "Eclipse Public License"
