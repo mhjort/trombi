81f62181de9657ecc2a166c43983c08b1a04fdbf
Markus Hjort <markus.hjort@gmail.com>
2022-02-18 17:57:05 +0200

    Bump prismatic.schema

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/core.async "1.5.648"]
                  [http-kit "2.5.3"]
-                 [prismatic/schema "1.1.12"]
+                 [prismatic/schema "1.2.0"]
                  [clojider-gatling-highcharts-reporter "0.3.2"]]
   :profiles {:dev {:global-vars {*warn-on-reflection* false}
                    :source-paths ["examples"]
