a260b8a14d10dc8f05e51a3150eca377eb158511
Markus Hjort <markus.hjort@gmail.com>
2022-05-05 17:09:01 +0300

    Bump dependencies in example

--- examples/metrics-simulation/project.clj
+++ examples/metrics-simulation/project.clj
@@ -3,6 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
-                 [clj-gatling "0.17.2"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [clj-gatling "0.17.3"]]
   :main metrics-simulation.core)
