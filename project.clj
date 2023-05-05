1c79a5c341f3698361338d4265f95a5487182284
Markus Hjort <markus.hjort@gmail.com>
2023-05-05 17:10:10 +0300

    Add logging libs to dev profile
    These can be used for debugging hard to track async/parallel bugs

--- project.clj
+++ project.clj
@@ -10,8 +10,12 @@
                  [clojider-gatling-highcharts-reporter "0.4.0"]]
   :profiles {:dev {:global-vars {*warn-on-reflection* false}
                    :source-paths ["examples"]
+                   :jvm-opts ["-Dclojure.tools.logging.factory=clojure.tools.logging.impl/log4j2-factory"]
                    :dependencies [[clj-time "0.15.2"]
                                   [clj-async-test "0.0.5"]
                                   [org.clojure/test.check "1.1.1"]
-                                  [clj-containment-matchers "1.0.1"]]}}
+                                  [clj-containment-matchers "1.0.1"]
+                                  [org.clojure/tools.logging "1.2.4"]
+                                  [org.apache.logging.log4j/log4j-api "2.19.0"]
+                                  [org.apache.logging.log4j/log4j-core "2.19.0"]]}}
   :aot [clj-gatling.simulation-runners])
