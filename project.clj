0294e0c11ffa69549c37a4fa82db23f374fe19ca
Markus Hjort <markus.hjort@gmail.com>
2023-06-02 17:14:02 +0300

    Bump dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject com.github.mhjort/trombi "1.0.0-beta2"
-  :description "Clojure library for load testing"
+  :description "Clojure library for load testing stateful apps"
   :url "http://github.com/mhjort/trombi"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/core.async "1.5.648"]
+                 [org.clojure/core.async "1.6.673"]
                  [prismatic/schema "1.4.1"]]
   :profiles {:dev {:global-vars {*warn-on-reflection* false}
                    :source-paths ["examples"]
@@ -16,6 +16,6 @@
                                   [org.clojure/test.check "1.1.1"]
                                   [clj-containment-matchers "1.0.1"]
                                   [org.clojure/tools.logging "1.2.4"]
-                                  [org.apache.logging.log4j/log4j-api "2.19.0"]
-                                  [org.apache.logging.log4j/log4j-core "2.19.0"]]}}
+                                  [org.apache.logging.log4j/log4j-api "2.20.0"]
+                                  [org.apache.logging.log4j/log4j-core "2.20.0"]]}}
   :aot [trombi.simulation-runners])
